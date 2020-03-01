679cfd0cd209c74ca452bfe18d0ea6f8cc1e6c5a
sham <s@istranet.ru>
2020-03-02 00:53:01 +0300

    переработан раздел статистика

--- status/text5.php
+++ status/text5.php
@@ -1,32 +1,55 @@
-<div align=center><b>Топ 10 бойцов на арене</b></div><br>
 <?php
-//print "<div align=center><b>Временно недоступен</b></div>";
-
-
-$time = time();
-$ct[0] = "Без города";
-$ct[1] = "Академия";
-$ct[2] = "Шамаал";
-$ct[3] = "Хроно";
-$ct[4] = "Иллюзив";
-$ct[5] = "Эндлер";
-$ct[6] = "Шелтер";
-$ct[14] = "Морок";
-$i = 0;
-print "<table cellspacing=1 cellpadding=2 width=98% bgcolor=95A7AA align=center><tr bgcolor=DEE6DF><td width=10>#</td><td width=33%  align=center><b>Имя</b></td><td  width=33% align=center><b>Город</b></td><td  align=center><b>Уровень</b></td><td align=center><b>Очки</b></td></tr>";
-
-$SQL="select name,city,level, rating from sw_users where rating <>100 and city <> 7 and npc = 0 and admin = 0 order by rating DESC limit 0,10";
-$row_num=SQL_query_num($SQL);
-while ($row_num){
-	$i++;
-	$name = $row_num[0];
-	$city = $row_num[1];
-	$level = $row_num[2];
-	$rating = $row_num[3];
-	print "<tr bgcolor=F7F7F7><td>$i</td><td width=33%  align=center><a href=fullinfo.php?name=$name target=_blank>$name</a></td><td  width=33% align=center>$ct[$city]</td><td  align=center>$level</td><td align=center>$rating</td></tr>";
-	$row_num=SQL_next_num();
-}
-if ($result)
-mysqli_free_result($result);
-print "</table>";
+    //Статистика -> топ бойцов
+    $ct = [
+        0 => 'Без города',
+        1 => 'Академия',
+        2 => 'Шамаал',
+        3 => 'Хроно',
+        4 => 'Иллюзив',
+        5 => 'Эндлер',
+        6 => 'Шелтер',
+        14 => 'Морок',
+    ];
+    $i = 0;
 ?>
+<div align="center"><b>Топ 10 бойцов на арене</b></div><br>
+<table cellspacing="1" cellpadding="2" width="98%" bgcolor="95A7AA" align="center">
+    <tr bgcolor="DEE6DF">
+        <td width="10">#</td>
+        <td width="33%" align="center"><b>Имя</b></td>
+        <td width="33%" align="center"><b>Город</b></td>
+        <td align="center"><b>Уровень</b></td>
+        <td align="center"><b>Очки</b></td>
+    </tr>
+    <?php
+    $SQL = "SELECT name, city, level, rating 
+            FROM sw_users 
+            WHERE rating <> 100 
+              AND city <> 7 
+              AND npc = 0 
+              AND admin = 0 
+            ORDER BY rating DESC
+            LIMIT 0, 10";
+    $row_num = SQL_query_num($SQL);
+    while ($row_num) {
+        $i++;
+        $name = $row_num[0];
+        $city = $row_num[1];
+        $level = $row_num[2];
+        $rating = $row_num[3];
+    ?>
+    <tr bgcolor="F7F7F7">
+        <td><?=$i?></td>
+        <td width="33%" align="center"><a href="fullinfo.php?name=<?=$name?>" target="_blank"><?=$name?></a></td>
+        <td width="33%" align="center"><?=$ct[$city]?></td>
+        <td align="center"><?=$level?></td>
+        <td align="center"><?=$rating?></td>
+    </tr>
+    <?php
+        $row_num=SQL_next_num();
+    }
+    if ($result) {
+        mysqli_free_result($result);
+    }
+    ?>
+</table>
--- status/text6.php
+++ status/text6.php
@@ -1,32 +1,46 @@
-<div align=center><b>Топ 10 по уровню </b></div><br>
 <?php
-//print "<div align=center><b>Временно недоступен</b></div>";
-
-
-
-$ct[0] = "Без города";
-$ct[1] = "Академия";
-$ct[2] = "Шамаал";
-$ct[3] = "Хроно";
-$ct[4] = "Иллюзив";
-$ct[5] = "Эндлер";
-$ct[6] = "Шелтер";
-$ct[14] = "Морок";
-$i = 0;
-print "<table cellspacing=1 cellpadding=2 width=98% bgcolor=95A7AA align=center><tr bgcolor=DEE6DF><td width=10>#</td><td width=33%  align=center><b>Имя</b></td><td  width=33% align=center><b>Город</b></td><td  align=center><b>Уровень</b></td></tr>";
-
-$SQL="select name,city,level from sw_users where level and city <> 7 and npc =0 and admin = 0 order by level DESC limit 0,10";
-$row_num=SQL_query_num($SQL);
-while ($row_num){
-	$i++;
-	$name = $row_num[0];
-	$city = $row_num[1];
-	$level = $row_num[2];
-
-	print "<tr bgcolor=F7F7F7><td>$i</td><td width=33%  align=center><a href=fullinfo.php?name=$name target=_blank>$name</a></td><td  width=33% align=center>$ct[$city]</td><td  align=center>$level</td></tr>";
-	$row_num=SQL_next_num();
-}
-if ($result)
-mysqli_free_result($result);
-print "</table>";
+    //Статистика->топ уровней
+    $ct = [
+        0 => 'Без города',
+        1 => 'Академия',
+        2 => 'Шамаал',
+        3 => 'Хроно',
+        4 => 'Иллюзив',
+        5 => 'Эндлер',
+        6 => 'Шелтер',
+        13 => 'Новый Локвуд',
+        14 => 'Морок',
+    ];
+    $i = 0;
 ?>
+<div align="center"><b>Топ 10 по уровню </b></div><br>
+<table cellspacing="1" cellpadding="2" width="98%" bgcolor="95A7AA" align="center">
+    <tr bgcolor="DEE6DF">
+        <td width="10">#</td>
+        <td width="33%" align="center"><b>Имя</b></td>
+        <td width="33%" align="center"><b>Город</b></td>
+        <td align="center"><b>Уровень</b></td>
+    </tr>
+    <?php
+    $SQL = "SELECT name, city, level FROM sw_users WHERE level AND city <> 7 AND npc = 0 AND admin = 0 ORDER BY level DESC LIMIT 0, 10";
+    $row_num = SQL_query_num($SQL);
+    while ($row_num) {
+        $i++;
+        $name = $row_num[0];
+        $city = $row_num[1];
+        $level = $row_num[2];
+    ?>
+	<tr bgcolor="F7F7F7">
+        <td><?=$i?></td>
+        <td width="33%" align="center"><a href="fullinfo.php?name=<?=$name?>" target="_blank"><?=$name?></a></td>
+        <td width="33%" align="center"><?=$ct[$city]?></td>
+        <td align="center"><?=$level?></td>
+    </tr>
+    <?php
+        $row_num=SQL_next_num();
+    }
+    if ($result) {
+        mysqli_free_result($result);
+    }
+    ?>
+</table>
